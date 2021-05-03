71aac4bc9fe1fce0f94881258f75b30bfb25e03b
Durraiz Azam <53301603+durraiz-azam@users.noreply.github.com>
2021-05-04 02:20:26 +0500

    Add files via upload

---
+++ filter-gallery.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $(".category-button").click(function(){
+        var filterValue = $(this).attr('data-filter');
+        if(filterValue == "all")      {
+            $(".all").hide("slow");
+            $(".all").show("slow");
+        }
+		else if(filterValue == "category1")    {
+            $(".all").hide("slow");
+            $(".category1").show("slow");
+        }
+		else if(filterValue == "category2") {
+            $(".all").hide("slow");
+            $(".category2").show("slow");
+        }
+		else if(filterValue == "category3") {
+            $(".all").hide("slow");
+            $(".category3").show("slow");
+        }
+		else if(filterValue == "category4")  {
+            $(".all").hide("slow");
+            $(".category4").show("slow");
+        }
+    });
+
+});
